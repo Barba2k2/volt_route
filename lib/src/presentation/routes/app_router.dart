1eacefaae7a5c50674b6e39274f62ad067172ec4
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-03 23:52:55 -0300

    feat: update app_router.dart

--- lib/src/presentation/routes/app_router.dart
+++ lib/src/presentation/routes/app_router.dart
@@ -1,24 +1,21 @@
-import 'package:go_router/go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../core/DI/dependency_injector.dart';
-import '../../domain/usecases/plan_route.dart';
 import '../../domain/usecases/get_saved_trips.dart';
+import '../../domain/usecases/plan_route.dart';
 import '../../domain/usecases/save_trip.dart';
-import '../../domain/usecases/authenticate_user.dart';
-import '../../domain/repositories/auth_repository.dart';
-import '../features/planning/planning_bloc.dart';
-import '../features/trips/trips_bloc.dart';
-import '../features/auth/auth_bloc.dart';
-import '../features/planning/views/planning_page.dart';
-import '../features/trips/views/trips_page.dart';
 import '../features/auth/views/auth_page.dart';
 import '../features/auth/views/profile_page.dart';
+import '../features/planning/planning_bloc.dart';
+import '../features/planning/views/planning_page.dart';
+import '../features/trips/trips_bloc.dart';
+import '../features/trips/views/trips_page.dart';
 
 /// App router configuration
 class AppRouter {
   static final GoRouter _router = GoRouter(
-    initialLocation: '/planning',
+    initialLocation: '/auth',
     routes: [
       GoRoute(
         path: '/planning',
@@ -44,24 +41,12 @@ class AppRouter {
       GoRoute(
         path: '/auth',
         name: 'auth',
-        builder: (context, state) => BlocProvider(
-          create: (context) => AuthBloc(
-            authenticateUser: getIt<AuthenticateUser>(),
-            authRepository: getIt<AuthRepository>(),
-          ),
-          child: const AuthPage(),
-        ),
+        builder: (context, state) => const AuthPage(),
       ),
       GoRoute(
         path: '/profile',
         name: 'profile',
-        builder: (context, state) => BlocProvider(
-          create: (context) => AuthBloc(
-            authenticateUser: getIt<AuthenticateUser>(),
-            authRepository: getIt<AuthRepository>(),
-          ),
-          child: const ProfilePage(),
-        ),
+        builder: (context, state) => const ProfilePage(),
       ),
     ],
   );
