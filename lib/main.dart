e9ab56487c78e43db8b28c2969bcaca30b382acb
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-03 23:03:08 -0300

    feat: update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'package:base_clean_arch_bloc/app_widget.dart';
-import 'package:base_clean_arch_bloc/src/core/DI/dependency_injector.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'src/core/di/dependency_injector.dart';
+import 'src/presentation/app_widget.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'pt_BR';
 
-  setupDependencyInjector(
-    /// Ao ativar essa flag, você vai ter um log das chamadas e respostas da API
-    /// no console do seu editor de codigo, por padrão é desabilitado.
-    loggerApi: true,
-  );
+  // Configure dependencies
+  await configureDependencies();
+
   runApp(const AppWidget());
 }
