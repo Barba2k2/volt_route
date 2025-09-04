6337dd8ced8f5d5ce93f35383d4e1725e90420d8
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-03 23:10:25 -0300

    feat: update credentials_validation_exception.dart

--- lib/src/core/errors/credentials_validation_exception.dart
+++ lib/src/core/errors/credentials_validation_exception.dart
@@ -1,5 +1,15 @@
 import 'base_exception.dart';
 
+/// Exception for credential validation errors
 class CredentialsValidationException extends BaseException {
-  CredentialsValidationException({required super.message});
+  final List<String> validationErrors;
+
+  const CredentialsValidationException({
+    required super.message,
+    this.validationErrors = const [],
+  });
+
+  @override
+  String toString() =>
+      'CredentialsValidationException: $message\nErrors: ${validationErrors.join(', ')}';
 }
