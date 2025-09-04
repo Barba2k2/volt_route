f768ae79c28b1d3efcaf4f6ba4ed7556e19d7222
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-03 23:07:39 -0300

    feat: update rest_client_exception.dart

--- lib/src/core/client_http/rest_client_exception.dart
+++ lib/src/core/client_http/rest_client_exception.dart
@@ -1,16 +1,17 @@
-import 'package:base_clean_arch_bloc/src/core/client_http/client_http.dart';
-import 'package:base_clean_arch_bloc/src/core/errors/errors.dart';
+import 'rest_client_http_message.dart';
+import 'rest_client_response.dart';
+import '../errors/base_exception.dart';
 
-class RestClientException extends BaseException implements RestClientHttpMessage {
-  dynamic error;
-  RestClientResponse? response;
+class RestClientException extends BaseException
+    implements RestClientHttpMessage {
+  final dynamic error;
+  final RestClientResponse? response;
+  final String? statusCode;
 
   RestClientException({
     required super.message,
-    super.statusCode,
-    super.data,
+    this.statusCode,
     required this.error,
     this.response,
-    super.stackTracing,
   });
 }
