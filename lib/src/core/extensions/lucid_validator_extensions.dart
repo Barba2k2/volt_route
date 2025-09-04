a3499d68a40980d5848d569ac227e8592659d672
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-03 23:11:22 -0300

    feat: update lucid_validator_extensions.dart

--- lib/src/core/extensions/lucid_validator_extensions.dart
+++ lib/src/core/extensions/lucid_validator_extensions.dart
@@ -1,6 +1,7 @@
-import 'package:base_clean_arch_bloc/src/core/errors/credentials_validation_exception.dart';
 import 'package:lucid_validation/lucid_validation.dart';
 import 'package:result_dart/result_dart.dart';
+
+import '../errors/credentials_validation_exception.dart';
 
 extension LucidValidatorExtensions<T extends Object> on LucidValidator<T> {
   AsyncResult<T> validateResult(T object) async {
@@ -19,9 +20,15 @@ extension CustomValidPasswordValidator on SimpleValidationBuilder<String> {
   SimpleValidationBuilder<String> customValidPassword() {
     return notEmpty(message: 'O campo senha é obrigatório')
         .minLength(8, message: 'A senha deve ter no mínimo 8 caracteres')
-        .mustHaveLowercase(message: 'A senha deve ter no mínimo uma letra minúscula')
-        .mustHaveUppercase(message: 'A senha deve ter no mínimo uma letra maiúscula')
+        .mustHaveLowercase(
+          message: 'A senha deve ter no mínimo uma letra minúscula',
+        )
+        .mustHaveUppercase(
+          message: 'A senha deve ter no mínimo uma letra maiúscula',
+        )
         .mustHaveNumber(message: 'A senha deve ter no mínimo um número')
-        .mustHaveSpecialCharacter(message: 'A senha deve ter no mínimo um caractere especial');
+        .mustHaveSpecialCharacter(
+          message: 'A senha deve ter no mínimo um caractere especial',
+        );
   }
 }
